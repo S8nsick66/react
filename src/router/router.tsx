70372e858b94babad0aa84f4b593ba48d58491ef
Dennis Germundal <dennis.germundal@prek.se>
2025-06-06 22:29:16 +0200

    main

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -6,24 +6,30 @@ import { ListingPage } from "../pages/ListingPage";
 import { Page } from "../pages/Page";
 import { ErrorPage } from "../pages/ErrorPage";
 
-export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Layout />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                index: true,
-                element: <ListingsPage />,
-            },
-            {
-                path: "listing/:id",
-                element: <ListingPage />,
-            },
-            {
-                path: ":slug",
-                element: <Page />
-            }
-        ],
-    },
-]);
+// Prefix each route with /react in production to work on GitHub Pages
+const basename = import.meta.env.PROD ? '/react' : '/';
+
+export const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <Layout />,
+            errorElement: <ErrorPage />,
+            children: [
+                {
+                    index: true,
+                    element: <ListingsPage />,
+                },
+                {
+                    path: "listing/:id",
+                    element: <ListingPage />,
+                },
+                {
+                    path: ":slug",
+                    element: <Page />
+                }
+            ],
+        },
+    ],
+    { basename }
+);
